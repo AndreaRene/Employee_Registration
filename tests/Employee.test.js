4b612d8477900cba9502613c6bcc92b9f845369d
Andrea <andrea@interhact.net>
2022-08-25 13:22:18 -0500

    commit before messing with queries file

---
+++ tests/Employee.test.js
@@ -0,0 +1,7 @@
+const Employee = require("../lib/Employee")
+
+describe("Employee Name", () => {
+    it("should do something", () => {
+        expect(new Employee("Kevin")).toBe(String); // TODO: replace placeholder method thing
+    });
+});
