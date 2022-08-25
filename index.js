3bd596101e801952bb32d33cbcbce3a5bc8b34bd
Andrea <andrea@interhact.net>
2022-08-25 18:24:30 -0500

    work done on classes

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Smile. You are enough.
+
+const inquirer = require("inquirer");
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+
+//global
+//const teamMemers = []
+
+// inside .then in inquirer
+// const team = new Manager(data.name, data.id, data.email, data.office)
+//teamMemers.push(team)
+
+
+// look up .filter
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,7 +1,23 @@
+// TODO: Smile. You are enough.
+
 const Employee = require("../lib/Employee")
 
-describe("Employee Name", () => {
-    it("should do something", () => {
-        expect(new Employee("Kevin")).toBe(String); // TODO: replace placeholder method thing
+describe("Employee", () => {
+    it("should return the name", () => {
+
+        // set variable
+        const name = "Kevin"
+        // put through constructor
+        const employee = new Employee(name)
+        // constructor returns constructed object
+        expect(employee.name).toBe(name);
     });
+    // should return id
+    // should return email
+
+    // test get name
+    // test get id
+    // test get email
+    // test get role
 });
+
