9593b72aa098fc54a779a0b6babee092e32aa2bd
Andrea <andrea@interhact.net>
2022-08-26 22:43:40 -0500

    work on class methods

---
+++ src/generateHTML.js
@@ -0,0 +1,30 @@
+// const Queries = require("../lib/queries")
+function generateHTML(teamMembers) {
+    return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" src="/dist/reset.css">
+    <link rel="stylesheet" src="/dist/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header id="pageHeader">
+    
+    </header>
+    <main id="pageMain">
+
+    </main>
+    <footer id="pageFooter">
+
+    </footer>
+</body>
+
+</html>`;
+
+}
+module.exports = generateHTML;
