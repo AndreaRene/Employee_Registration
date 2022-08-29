2b8000f282815e3f35909fe7fc8ca07feaddd832
Andrea <andrea@interhact.net>
2022-08-29 15:24:27 -0500

    working on styling with bootstrap

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -19,17 +19,18 @@ function cardLoop(teamMembers) {
 };
 
 function generateCards(employee) {
-    return `<div class="card">
-        <header class="cardHeader">
-            <h2>${employee.name}</h2>
-            <h3>${employee.role}</h3>
-        </header>
-        <section class="cardSection">
-            <p>Email: <a href="mailto:${employee.email}" target="_">${employee.email}</a></p>
-            <p>ID: ${employee.id}</p>
-            <p>${getProp(employee)}</p>
-        </section>
-    </div> `;
+    return `<div class="card" style="width: 15rem;">
+    <div class="card-body">
+        <h5 class="card-title">${employee.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${employee.role}</h6>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">Email: <a href="mailto:${employee.email}" target="_">${employee.email}</a>
+        </li>
+        <li class="list-group-item">EID: ${employee.id}</li>
+        <li class="list-group-item">${getProp(employee)}</li>
+    </ul>
+    </div>
+</div>`;
 };
 
 function generateHTML(teamMembers) {
@@ -38,11 +39,16 @@ function generateHTML(teamMembers) {
         <html lang="en">
 
             <head>
-                <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <link rel="stylesheet" src="/dist/reset.css">
-                                <link rel="stylesheet" src="/dist/style.css">
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+                crossorigin="anonymous"></script>
+            <link rel="stylesheet" src="/dist/reset.css">
+            <link rel="stylesheet" src="/dist/style.css">
                                     <title>${teamMembers[0].name}'s Team</title>
                                 </head>
 
@@ -52,7 +58,7 @@ function generateHTML(teamMembers) {
                                     </header>
 
                                     <main id="pageMain">
-                                        <section id="cards">
+                                        <section id="cards d.flex">
                                             ${cardLoop(teamMembers)}
                                         </section>
                                     </main>
